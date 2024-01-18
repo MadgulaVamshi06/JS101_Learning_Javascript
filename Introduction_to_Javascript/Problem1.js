cbd7c6e2c79193319766c54c2239af4dbaad4df5
MadgulaVamshi06 <vamshimadgula@gmail.com>
2024-01-18 21:40:46 +0530

    second problem

--- Introduction_to_Javascript/Problem1.js
+++ Introduction_to_Javascript/Problem1.js
@@ -1,3 +1,4 @@
 var name = "Masai School";
 console.log(name);
 console.log("A Transformation in Education");
+
---
+++ Introduction_to_Javascript/Problem2.js
@@ -0,0 +1,6 @@
+var name = "MADGULA VAMSHI";
+console.log(name);
+name = "M THIRUPATHAIAH";
+console.log(name);
+name = "K LALITHA";
+console.log(name);
