d152dd2181aa0f056bdca3c10c3a6609fc4460d1
MadgulaVamshi06 <vamshimadgula@gmail.com>
2024-01-24 10:30:33 +0530

    added swapedstr fun

---
+++ Functions/pb6.js
@@ -0,0 +1,50 @@
+function upperCase(x){  
+    let lower="abcdefghijklmnopqrstuvwxyz";
+    let upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for(let i=0;i<lower.length;i++)
+    {
+        if(x==lower[i])
+        {
+          return upper[i];
+        }
+    }
+      return x;
+    }
+  function lowerCase(x){  
+  let lower="abcdefghijklmnopqrstuvwxyz";
+  let upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for(let i=0;i<lower.length;i++)
+  {
+      if(x==upper[i])
+      {
+        return lower[i];
+      }
+  }
+    return x;
+  }
+  
+    function swap(inputString){
+    var result = "";
+      for (var i = 0; i < inputString.length; i++) 
+      {
+      var currentChar = inputString[i];
+      if (currentChar >= 'A' && currentChar <= 'Z') 
+      { 
+        result = result + lowerCase(currentChar);
+       } 
+      else if (currentChar >= 'a' && currentChar <= 'z') 
+      { 
+        result = result + upperCase(currentChar);
+      } 
+      else 
+      {
+        result = result + currentChar;
+      }
+    }
+        return result;
+  }
+  var str = "Test";
+  console.log(str);
+  var swappedString = swap(str);
+  console.log(swappedString);
+  
