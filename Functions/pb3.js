b186eae5ed4b31a68dfcce8ea58e27fc6686dab2
MadgulaVamshi06 <vamshimadgula@gmail.com>
2024-01-24 10:25:49 +0530

    added odd logic function

---
+++ Functions/pb3.js
@@ -0,0 +1,16 @@
+function isOdd(num){
+    for(var i = 0; i<=num; i++){
+    if(num%2==1){
+      return true;
+    }
+    else{
+      return
+  }
+  }
+  }
+  for(var i = 1; i<=10; i++){
+    var output = isOdd(i);
+    if(output == true){
+      console.log(i);
+    }
+  } 
